d15728469d6ae931ba5f1f3d287bee89d0849270
Gagan-700 <gagan.jaiswal2@gmail.com>
2021-08-06 09:56:01 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,16 @@ export class AppComponent  {
 
   gender: string;
   country: string;
+  mpc: string;
 
   countries: string[] = ['Korea', 'Japan', 'USA', 'London', 'Philippines', 'Thailand'];
   
   users: any[] = [
-    { name: 'Kristy', gender: 'female', country: 'Korea' },
-    { name: 'Nick', gender: 'male', country: 'Japan' },
-    { name: 'Ariana', gender: 'female', country: 'USA' },
-    { name: 'Joe', gender: 'male', country: 'London' },
-    { name: 'Albert', gender: 'male', country: 'Philippines' },
+    { name: 'Kristy', gender: 'female', country: 'Korea',mpc:"001" },
+    { name: 'Nick', gender: 'male', country: 'Japan',mpc:"001" },
+    { name: 'Ariana', gender: 'female', country: 'USA',mpc:"002" },
+    { name: 'Joe', gender: 'male', country: 'London',mpc:"001" },
+    { name: 'Albert', gender: 'male', country: 'Philippines',mpc:"002" },
   ];
 
 }
